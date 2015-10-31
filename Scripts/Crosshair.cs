920278e8a597c82569b1e2483df307d53292826a
unknown <kag4380@IGMGRAD-16.main.ad.rit.edu>
2015-10-30 20:35:17 -0400

    First commit

---
+++ Scripts/Crosshair.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+	Ray ray;
+	public Camera fpsCam;
+	public GameObject FPS;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var directionRay = transform.forward;
+		ray = fpsCam.ScreenPointToRay(transform.position);
+		Debug.Log (ray.origin);
+		Debug.Log (ray.direction);
+		Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+		RaycastHit hit;
+		if(Physics.Raycast(ray, out hit, Mathf.Infinity)){
+
+			if(Input.GetMouseButton(0)){
+				hit.transform.parent = FPS.transform;
+			}
+			Debug.Log (hit.transform.gameObject.name);
+		}
+	
+	}
+}
